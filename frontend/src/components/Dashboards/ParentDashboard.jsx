e7916c5656af85b4520b90f19f9bb6ccac622195
Nati-darse <natnaeldarsema@gmail.com>
2025-08-14 23:16:33 +0300

    prepare a dashboard for different roles

---
+++ frontend/src/components/Dashboards/ParentDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ParentDashboard = () => {
+  return (
+    <dev>
+    </dev>
+  );
+}
+export default ParentDashboard;
---
+++ frontend/src/components/Dashboards/TeacherDashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react',
+
+const  TeacherDashboard = () => {
+  return (
+<dev></dev>
+  );
+}
+  export default TeacherDashboard;
